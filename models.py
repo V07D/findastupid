c344251ba1728ac081cad8c86854f27f3ec080a0
perevedko <alex45510@ya.ru>
2014-07-25 03:49:08 +0400

    user, session -> models.py

--- models.py
+++ models.py
@@ -1,1 +1,13 @@
 # coding: utf-8
+
+class User:
+	__init__(self):
+		self._name = ''
+		self._surname = ''
+	
+class Session: 
+	_state = 'INACTIVE'
+	def __init__(self,users):
+		self._users = users
+	
+	
--- session.py
+++
@@ -1,6 +0,0 @@
-class Session: 
-	_state = 'INACTIVE'
-	def __init__(self,users):
-		self._users = users
-	
-	
