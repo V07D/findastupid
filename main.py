10861265797c36653d62917d34f1aafd6947e2b5
perevedko <alex45510@ya.ru>
2014-07-25 06:53:59 +0400

    some magic and fixes in main.py

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
-	#!/usr/bin/python3
+#!/usr/bin/python3
 # coding: utf-8
 # таки на питончике третьем все работает
-from flask import Flask
-from flask import render_template
-from flask import json
-from flask import session, redirect, url_for, escape, request
+from flask import (
+	Flask, render_template, json, session, redirect, url_for, escape, request, send_from_directory
+)
 import logging
 import hashlib
+import os
+from functools import reduce # бугага
+from operator import add # бвзазваава
 
 app = Flask(__name__)
 
 m = hashlib.md5()
 
-user = {"login":"test","password":"test"}
+def makehash(*args):
+	"""sha1(sha1(arg1) + sha1(arg2) + ... + sha1(argn))"""
+	# потому что я могу
+	sha1 = hashlib.sha1
+	sequence = [sha1(n.encode('utf-8')).hexdigest() for n in args]
+	return sha1(reduce(add, sequence, '').encode('utf-8')).hexdigest()
+
+
+user = {'login': 'test', 'password': 'test'}
 
 @app.route("/")
 def index():
-    return render_template('index.html',logged=int(bool(session.get('logged'))))
+    return render_template('index.html',
+		logged=int(bool(session.get('logged')))
+	)
 
-@app.route("/login",methods=['post'])
+@app.route("/login", methods=['POST'])
 def login():
 	email = request.form['email']
 	password = request.form['password']
 	if email == user['login'] and password == user['password']:
-		app.logger.info('Email: %s' % (email,))
-		m.update(email+password)
+		# '%s' % s <- валидно
+		app.logger.info('Email: %s' % email)
+		m.update(email + password)
 		sid = m.digest()
+		# не совсем понял, зачем тебе sid
 		session['logged'] = 1
-		app.logger.info('sid: %s' % (sid,))
+		app.logger.info('sid: %s' % sid)
 	else:
 		app.logger.info('login incorrect')
 		session['logged'] = 0
@@ -39,15 +53,20 @@ def game(gid):
 	
 @app.route("/json/questions")
 def questions():
-	if bool(session.get('logged')):	
-		with open('static/json/questions.json') as json:
-			return json.read()
+	if session.get('logged'):	
+		#with open('./static/json/questions.json') as json:
+		#	return json.read()
+		# депрекатед дуе то некрасиво
+		# os.path.join, по хорошему, нужен
+		filename = os.path.join('json', 'questions.json')
+		# /static/json/questions.json
+		return app.send_static_file(filename)
 	else:
 		return 'You are not logged in, sorry!'
 		
 @app.route("/json/question/<id>")
 def getQuestion(qid):
-	if bool(session.get('logged')):	
+	if session.get('logged'):	
 		return 'We will get question from DB by qid here'
 	else:
 		return 'You a not logged in, sorry!'
@@ -57,4 +76,6 @@ def getGameSession(gid):
 
 if __name__ == "__main__":
 	app.secret_key = '9sabdf9(B&F(B9fa0bdb(&D(S&(0dbfas[f9'
-	app.run(debug=True)
+	# ключ надо генерировать, это очевидно, но можно и забыть
+	app.debug = True
+	app.run()
